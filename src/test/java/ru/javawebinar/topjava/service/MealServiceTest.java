17c3f2f7ce66ed3f5e9248455f2d48e004b248b6
Igor <naslednikg@gmail.com>
2018-08-26 14:17:59 +0300

    5 2 fix hibernate issue

--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -1,5 +1,7 @@
 package ru.javawebinar.topjava.service;
 
+import org.hsqldb.lib.StopWatch;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.bridge.SLF4JBridgeHandler;
@@ -29,6 +31,11 @@ public class MealServiceTest {
     static {
         SLF4JBridgeHandler.install();
     }
+
+    @Rule
+    StopWatch watch = new StopWatch(){
+
+    };
 
     @Autowired
     private MealService service;
